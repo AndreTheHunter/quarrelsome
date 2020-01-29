64b6bea0b38a32d3a654af18bbc92f954ad54474
André de Jager <andrethehunter@gmail.com>
2020-01-29 10:19:00 +1000

    Lazy deps for quarrelsome.clj

--- quarrelsome.clj
+++ quarrelsome.clj
@@ -1,9 +1,6 @@
 #!/usr/bin/env cljog
 (deps '[[com.clojure-goes-fast/lazy-require "0.1.1"]
-        [quarrelsome "0.1.0-SNAPSHOT"]
-        ;TODO load lazily once cljog supports lazy deps
-        [leiningen "2.9.1"]
-        [clj-jgit "1.0.0-beta3"]])
+        [quarrelsome "0.1.0-SNAPSHOT"]])
 (ns quarrelsome
   "Quarrelsome build tool"
   (:refer-clojure :exclude [test])
@@ -11,34 +8,32 @@
     [quarrel.core :refer [run]]
     [lazy-require.core :refer [with-lazy-require]]))
 
-(def echo println)
+(def ^:private echo println)
 
 (defn- err [& more]
   (binding [*out* *err*]
     (apply println more)))
 
-(defn- project
-  ([] (project [:default]))
-  ([profiles]
-   (with-lazy-require [[leiningen.core.project :as project]]
-     (project/ensure-dynamic-classloader)
-     (project/read "project.clj" profiles))))
-
-(defn- -lein [project args]
-  (with-lazy-require [[clojure.stacktrace :as stacktrace]
-                      [clojure.string :as str]
-                      [leiningen.core.main :as lein]]
-    (try
-      (echo "Running:" "lein" (str/join \space args))
-      (lein/resolve-and-apply project args)
-      (catch Exception e
-        (stacktrace/print-cause-trace e)))))
+(defn- -lein
+  ([args] (-lein [:default] args))
+  ([profiles args]
+   (user/deps '[[leiningen "2.9.1"]])
+   (with-lazy-require [[clojure.stacktrace :as stacktrace]
+                       [clojure.string :as str]
+                       [leiningen.core.main :as lein]
+                       [leiningen.core.project :as project]]
+     (echo "Running:" "lein" (str/join \space args))
+     (try
+       (project/ensure-dynamic-classloader)
+       (lein/resolve-and-apply (project/read "project.clj" profiles) args)
+       (catch Exception e
+         (stacktrace/print-cause-trace e))))))
 
 (defn- lein [& args]
-  (-lein (project) args))
+  (-lein args))
 
 (defn- lein-dev [& args]
-  (-lein (project [:dev]) args))
+  (-lein [:dev] args))
 
 (defn deps [_]
   (lein "-U" "deps"))
@@ -57,15 +52,15 @@
               "test")))
 
 (defn- require-committed [& paths]
-  (with-lazy-require [[clj-jgit.porcelain :as git]
-                      [leiningen.core.main :as lein]]
+  (user/deps '[[clj-jgit "1.0.0-beta3"]])
+  (with-lazy-require [[clj-jgit.porcelain :as git]]
     (when (->> (git/git-status (git/load-repo ".") :paths paths)
             vals
             (some seq))
       (err "Uncommitted files in" paths)
-      (lein/exit 1))))
+      (System/exit 1))))
 
-(defn docs "generate API docs"[_]
+(defn docs "generate API docs" [_]
   (lein-dev "docs"))
 
 (defn test-docs [_]
